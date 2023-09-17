3168296cd1cecdaa84ccd07ab6fcf6e163e39901
s4il3sh <144289804+s4il3sh@users.noreply.github.com>
2023-09-17 12:50:51 -0400

    quantumcircuit docstring added

--- quantum_addition.py
+++ quantum_addition.py
@@ -16,43 +16,74 @@ def add_quantum(num1, num2, shots=1024):
     # Determine the number of qubits needed to represent the numbers
     max_value = max(num1, num2)
     num_qubits = max(1, (max_value.bit_length() + 1))
+class QuantumCircuit:
+    """
+    QuantumCircuit class represents a quantum circuit for quantum computing.
 
+    Attributes:
+        num_qubits (int): The number of qubits in the quantum circuit.
+        data (list): A list of quantum gates and operations applied to the circuit.
+    
+    Methods:
+        add_gate(gate, target_qubits, *params):
+            Add a quantum gate to the circuit.
+
+        measure(qubits, classical_bits):
+            Measure specified qubits and store the results in classical bits.
+
+        execute(backend, shots=1):
+            Execute the quantum circuit on a specified quantum backend.
+
+    Example usage:
+    ```
+    # Create a quantum circuit with 3 qubits
+    circuit = QuantumCircuit(3)
+    
+    # Add quantum gates to the circuit
+    circuit.add_gate(HadamardGate(), [0])
+    circuit.add_gate(CNOTGate(), [0, 1])
+    
+    # Measure qubits and execute on a quantum backend
+    circuit.measure([0, 1], [0, 1])
+    results = circuit.execute(quantum_simulator_backend, shots=1000)
+    ```
+    """
     # Create a quantum circuit with enough qubits
-    qc = QuantumCircuit(num_qubits * 2, num_qubits)
-
-    # Encode the classical numbers into quantum states
-    for i in range(num_qubits):
-        if (num1 >> i) & 1:
-            qc.x(i)  # Apply X gate for 1 bits in num1
-        if (num2 >> i) & 1:
-            qc.x(i + num_qubits)  # Apply X gate for 1 bits in num2
-
-    # Perform the addition by applying CNOT gates
-    for i in range(num_qubits - 1):
-        qc.ccx(i, i + num_qubits, i + num_qubits + 1)
-        qc.cx(i, i + num_qubits)
-
-    # Measure the result
-    qc.measure(range(num_qubits, num_qubits * 2), range(num_qubits))
-
-    # Simulate the circuit
-    simulator = Aer.get_backend('qasm_simulator')
-    compiled_circuit = transpile(qc, simulator)
-    job = execute(compiled_circuit, simulator, shots=shots)  # Specify the number of shots
-    result = job.result()
-
-    # Get the measurement result
-    counts = result.get_counts(qc)
-    result_decimal = int(list(counts.keys())[0], 2)
-
-    return result_decimal, counts  # Return both the result and the counts
-
-# Example usage
-number1 = 3
-number2 = 2
-shots = 1024  # Specify the number of shots
-result, counts = add_quantum(number1, number2, shots=shots)
-print(f"The result of {number1} + {number2} is {result}")
-
-# Plot histogram of measurement outcomes
-plot_histogram(counts)
+        qc = QuantumCircuit(num_qubits * 2, num_qubits)
+    
+        # Encode the classical numbers into quantum states
+        for i in range(num_qubits):
+            if (num1 >> i) & 1:
+                qc.x(i)  # Apply X gate for 1 bits in num1
+            if (num2 >> i) & 1:
+                qc.x(i + num_qubits)  # Apply X gate for 1 bits in num2
+    
+        # Perform the addition by applying CNOT gates
+        for i in range(num_qubits - 1):
+            qc.ccx(i, i + num_qubits, i + num_qubits + 1)
+            qc.cx(i, i + num_qubits)
+    
+        # Measure the result
+        qc.measure(range(num_qubits, num_qubits * 2), range(num_qubits))
+    
+        # Simulate the circuit
+        simulator = Aer.get_backend('qasm_simulator')
+        compiled_circuit = transpile(qc, simulator)
+        job = execute(compiled_circuit, simulator, shots=shots)  # Specify the number of shots
+        result = job.result()
+    
+        # Get the measurement result
+        counts = result.get_counts(qc)
+        result_decimal = int(list(counts.keys())[0], 2)
+    
+        return result_decimal, counts  # Return both the result and the counts
+    
+    # Example usage
+    number1 = 3
+    number2 = 2
+    shots = 1024  # Specify the number of shots
+    result, counts = add_quantum(number1, number2, shots=shots)
+    print(f"The result of {number1} + {number2} is {result}")
+    
+    # Plot histogram of measurement outcomes
+    plot_histogram(counts)
